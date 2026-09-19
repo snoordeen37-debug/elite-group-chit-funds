9bad2d8c9ce26435a6c2856f23db2ddc1f736cca
snoordeen37-debug <snoordeen37@gmail.com>
2026-09-19 12:14:32 +0530

    Versal updation

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 import './i18n';
@@ -7,5 +8,6 @@ import './i18n';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
